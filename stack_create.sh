6b7a61fd648a29ff87e7a33d70b309b03c738c69
LeopoldWalther <leopold.walther@gmail.com>
2023-07-08 18:40:18 +0200

    fix: cluster version

--- stack_create.sh
+++ stack_create.sh
@@ -1,7 +1,7 @@
-ENVIRONMENT_NAME="vlc"
-STACK_NAME="network"
-TEMPLATE_FILE="infrastructure/vlc-network.yml"
-PARAMETER_FILE="file://infrastructure/vlc-network-parameters.json"
+ENVIRONMENT_NAME="vlc-real-estate-app"
+STACK_NAME="cluster"
+TEMPLATE_FILE="infrastructure/eks-cluster.yml"
+PARAMETER_FILE="file://infrastructure/eks-cluster-parameters.json"
 DEFAULT_REGION="us-east-1"
 
 aws cloudformation deploy \
@@ -10,4 +10,3 @@ aws cloudformation deploy \
   --parameter-overrides $PARAMETER_FILE \
   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
   --region $DEFAULT_REGION
-
